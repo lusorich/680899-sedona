14ce5cfc64410766c0b07449e1b098d77298e05f
Lusorich <dreamonautre@gmail.com>
2018-12-16 17:46:17 +0500

    Сделана сложная форма, стайлгайд,начало js

---
+++ js/script.js
@@ -0,0 +1,4 @@
+var btn = document.querySelector(".search-hotel-btn");
+var popup = document.querySelector(".search-hotel-popup")
+
+btn.addEventListener("click", f = () => {popup.classList.toggle("show")});
