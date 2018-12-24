5f88131d183a45f5209f8b5d6d1424200e8aef73
lusorich <dreamonautre@gmail.com>
2018-12-24 15:16:43 +0500

    Доработан js, правки по пиксель перфекту, добавлены нормальные тире, переделаны свг

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ var btn = document.querySelector(".search-hotel-btn");
 var popup = document.querySelector(".search-hotel-popup-wrapper")
 
 btn.addEventListener("click",function (){popup.classList.toggle("show")});
+
+document.addEventListener("DOMContentLoaded", function() {
+    popup.classList.remove("show");
+  });
